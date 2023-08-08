60e9d1f4b66ff735539a8c8cabfcb486c42b4388
Kaique Ferreira <kakamelo141205@gmail.com>
2023-08-08 15:42:46 -0300

    init reposity, add images, styled-components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
-
+/** @format */
+import React, { useState } from "react";
+import { AiOutlineSearch, AiFillBell, AiOutlineHeart } from "react-icons/ai";
+import {
+	Header,
+	HeaderView,
+	NameMarca,
+	ContainerSearch,
+	Main,
+	ContainerMain,
+	ContainerVertical,
+	TextVertical,
+	ContainerPalet,
+	PaletBoot,
+} from "./styled.js";
+import "./assets/style/reset.css";
+import "./assets/style/style.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [touchStartX, setTouchStartX] = useState(null);
+	const [scrollLeft, setScrollLeft] = useState(0);
+
+	const handleTouchStart = (e) => {
+		setTouchStartX(e.touches[0].clientX);
+	};
+
+	const handleTouchMove = (e) => {
+		if (touchStartX !== null) {
+			const touchMoveX = e.touches[0].clientX;
+			const scrollAmount = touchStartX - touchMoveX;
+			setScrollLeft(scrollLeft + scrollAmount);
+			setTouchStartX(touchMoveX);
+		}
+	};
+
+	const handleTouchEnd = () => {
+		setTouchStartX(null);
+	};
+	return (
+		<div className='App'>
+			<Header>
+				<HeaderView>
+					<NameMarca>Discover</NameMarca>
+					<ContainerSearch>
+						<AiOutlineSearch size={25} />
+						<AiFillBell size={25} />
+					</ContainerSearch>
+				</HeaderView>
+			</Header>
+			<Main>
+				<ContainerMain
+					onTouchStart={handleTouchStart}
+					onTouchMove={handleTouchMove}
+					onTouchEnd={handleTouchEnd}>
+					<h3>Nike</h3>
+					<h3>Adidas</h3>
+					<h3>Jordan</h3>
+					<h3>Puma</h3>
+					<h3>Gucci</h3>
+				</ContainerMain>
+			</Main>
+			<ContainerVertical>
+				<TextVertical>
+					<h3>New</h3>
+					<h3>Featured</h3>
+					<h3>Upcoming</h3>
+				</TextVertical>
+				<ContainerPalet
+					onTouchStart={handleTouchStart}
+					onTouchMove={handleTouchMove}
+					onTouchEnd={handleTouchEnd}>
+					<PaletBoot>
+						<div className='boot-header'>
+							<h3>Nike</h3>
+							<AiOutlineHeart size={15} />
+						</div>
+						<div className='main-boot'>
+							<h3>AIR-MAX</h3>
+							<span>$130.00</span>
+						</div>
+						<div className='image-boot'></div>
+					</PaletBoot>
+					<PaletBoot>
+						<div className='boot-header'>
+							<h3>Nike</h3>
+							<AiOutlineHeart size={15} />
+						</div>
+						<div className='main-boot'>
+							<h3>AIR-MAX</h3>
+							<span>$130.00</span>
+						</div>
+						<div className='image-boot'></div>
+					</PaletBoot>
+					<PaletBoot>
+						<div className='boot-header'>
+							<h3>Nike</h3>
+							<AiOutlineHeart size={15} />
+						</div>
+						<div className='main-boot'>
+							<h3>AIR-MAX</h3>
+							<span>$130.00</span>
+						</div>
+						<div className='image-boot'></div>
+					</PaletBoot>
+				</ContainerPalet>
+			</ContainerVertical>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/styled.js
@@ -0,0 +1,94 @@
+/** @format */
+
+import styled from "styled-components";
+
+export const Header = styled.header`
+	background: rgba(255, 255, 255, 1);
+`;
+
+export const HeaderView = styled.div`
+	margin: 10px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+export const NameMarca = styled.h3`
+	color: #212121;
+	font-family: Verdana, Geneva, Tahoma, sans-serif;
+	font-size: 8vw;
+`;
+
+export const ContainerSearch = styled.div`
+	display: flex;
+	gap: 20px;
+`;
+
+export const Main = styled.main`
+	margin-top: 30px;
+`;
+
+export const ContainerMain = styled.div`
+	display: flex;
+	align-items: center;
+	white-space: nowrap;
+	overflow-x: auto;
+	margin: 25px 20px;
+	gap: 20px;
+	h3 {
+		border: none;
+		background: transparent;
+		font-size: 7vw;
+		font-weight: 800;
+		font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+			"Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+		color: rgba(170, 170, 170, 1);
+		cursor: pointer;
+	}
+`;
+
+export const ContainerVertical = styled.div`
+	display: flex;
+	margin: 30px 0;
+	display: flex;
+	align-items: center;
+`;
+
+export const TextVertical = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	gap: 20px;
+	transform: rotate(-180deg);
+	margin: 0 25px;
+	h3 {
+		writing-mode: vertical-lr;
+		word-spacing: 5px;
+		text-orientation: mixed;
+		line-height: 25px;
+		white-space: nowrap;
+		font-size: 7vw;
+		font-weight: 800;
+		font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+			"Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+		color: rgba(170, 170, 170, 1);
+		cursor: pointer;
+	}
+`;
+export const ContainerPalet = styled.div`
+	display: flex;
+	align-items: center;
+	white-space: nowrap;
+	overflow-x: visible;
+	gap: 40px;
+	margin: 0 0 0 10px;
+`;
+export const PaletBoot = styled.div`
+	background: #909090;
+	width: 240px;
+	height: 335px;
+	/* overflow-x: auto; */
+	border-radius: 30px;
+	display: flex;
+	flex-direction: column;
+`;
